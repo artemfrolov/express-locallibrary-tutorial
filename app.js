d980688c88938c57febebefff5b562a29cef9713
artemfrolov <Sullankiri@gmail.com>
2024-02-24 19:12:49 +0200

    Added models, removed last to npms, added mongoose

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://admin:Ln3egLVtK0YUutzv@cluster0.j47ak7k.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
